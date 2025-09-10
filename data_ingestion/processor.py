1e051745a4fcdb6c7d06e9c804cd3bebcbb51ea4
Daniel Fisher <danielg16fisher@gmail.com>
2025-09-10 10:29:50 +0300

    BDS analysis function added to processor

--- data_ingestion/processor.py
+++ data_ingestion/processor.py
@@ -1,14 +1,19 @@
 import json
 import speech_recognition as sr
 from datetime import datetime
+from utils.config import NEUTRAL_WORDS, HOSTILE_WORDS
 from utils.load_data import DataLoader
 from utils.logger import Logger
+from utils.text_file_manager import TextFileManager
 
 
 class Processor:
     def __init__(self):
         self.recognizer = sr.Recognizer()
         self.logger = Logger.get_logger()
+        self.tfm = TextFileManager()
+        self.neutral_words = self.load_word_list(NEUTRAL_WORDS)
+        self.hostile_words = self.load_word_list(HOSTILE_WORDS)
 
     def get_file_name(self, wav_file_path):
         """
@@ -25,6 +30,44 @@ class Processor:
         except Exception as e:
             self.logger.error(f"An error occurred: {e}")
 
+    def load_word_list(self, path):
+        """
+        Get a .txt file path and extract the words before putting
+        them in a list
+        :param path: .txt file path
+        :return: List of strings from .txt file or empty list
+        """
+        if path:
+            content = self.tfm.extract_from_txt_file(path)
+            if content:
+                return [w.strip().lower() for w in content.split("\n") if w.strip()]
+            return []
+
+    def analyse_bds(self, text):
+        """
+        Go through text and determine if it classifies as BDS
+        (Boycott, Divest, Sanction) and provide ranking and
+        sentiment rank keyword
+        :param text: Text derived from WAV file
+        :return: bds_percent - number rank of text
+                 is_bds - text classifies as bds
+                 bds_threat_level - keyword rank of text
+        """
+
+        text_lower = text.lower()
+        total_words = len(text_lower.split()) or 1
+        hostile_count = sum(text_lower.count(w) for w in self.neutral_words)
+        neutral_count = sum(text_lower.count(w) for w in self.hostile_words)
+        bds_percent = (hostile_count + neutral_count) / total_words * 100
+        is_bds = bds_percent > 2
+        if bds_percent <= 2:
+            bds_threat_level = "None"
+        elif 2 < bds_percent >= 5:
+            bds_threat_level = "Neutral"
+        else:
+            bds_threat_level = "Hostile"
+
+        return bds_percent, is_bds, bds_threat_level
 
     def get_text_from_wav(self,wav_file_path):
         """
@@ -85,15 +128,23 @@ class Processor:
         Creates a single JSON object to send with all the necessary data
         :return: JSON object full of Audio file links and metadata
         """
+        content = self.get_text_from_wav(file_path)
 
-        data = {"wav_file_link": str(file_path),
-                "metadata": {
-                    "file_name": self.get_file_name(file_path),
-                    "date_of_creation": self.get_file_creation_date(file_path),
-                    "size": self.get_file_size(file_path),
-                    "text": self.get_text_from_wav(file_path)
+        bds_percent, is_bds, bds_threat_level = self.analyse_bds(content)
+        data = {
+                "wav_file_link": str(file_path),
+                    "metadata": {
+                        "file_name": self.get_file_name(file_path),
+                        "date_of_creation": self.get_file_creation_date(file_path),
+                        "size": self.get_file_size(file_path),
+                    },
+                    "text": {
+                        "content": content,
+                        "bds_percent": bds_percent,
+                        "is_bds": is_bds,
+                        "bds_threat_level": bds_threat_level
+                    }
                 }
-            }
 
         data = json.dumps(data, indent=4)
         self.logger.info(f"Extracted {data} from {file_path} WAV file")
